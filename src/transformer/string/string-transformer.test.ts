155a16ed84b832178137fd39dacb9460be371265
Dmitry Demensky <dmitry@demensky.com>
2021-07-18 14:55:47 +0300

    Simplify string-transformer.test.ts

--- src/transformer/string/string-transformer.test.ts
+++ src/transformer/string/string-transformer.test.ts
@@ -6,27 +6,6 @@ import {IncompatibleLiteralError} from '../../error/incompatible-literal-error';
 
 import {StringTransformer} from './string-transformer';
 
-const TEST_ZERO_NUMBER = 0;
-const TEST_ONE_NUMBER = 1;
-
-const INVALID_VALUES = [
-  null,
-  undefined,
-  TEST_ZERO_NUMBER,
-  TEST_ONE_NUMBER,
-  [],
-  false,
-  true,
-] as const;
-
-const VALID_VALUES = [
-  '',
-  '\0', // \u0000
-  '\r\n',
-  'foo',
-  '\ud83d', // broken unicode
-] as const;
-
 describe('StringTransformer', () => {
   let transformer: StringTransformer;
 
@@ -34,19 +13,28 @@ describe('StringTransformer', () => {
     transformer = new StringTransformer();
   });
 
-  describe.each(INVALID_VALUES)('invalid %p', (data) => {
-    test('compatible', () => {
-      expect(transformer).not.toBeCompatibleWith(data);
-    });
+  describe.each([null, undefined, 0, 1, [], false, true])(
+    'invalid %p',
+    (data) => {
+      test('compatible', () => {
+        expect(transformer).not.toBeCompatibleWith(data);
+      });
 
-    test('fromLiteral', () => {
-      expect(() => {
-        transformer.fromLiteral(data);
-      }).toThrow(IncompatibleLiteralError);
-    });
-  });
+      test('fromLiteral', () => {
+        expect(() => {
+          transformer.fromLiteral(data);
+        }).toThrow(IncompatibleLiteralError);
+      });
+    },
+  );
 
-  describe.each(VALID_VALUES)('valid %p', (data) => {
+  describe.each([
+    '',
+    '\0', // \u0000
+    '\r\n',
+    'foo',
+    '\ud83d', // broken unicode
+  ])('valid %p', (data) => {
     test('compatible', () => {
       expect(transformer).toBeCompatibleWith(data);
     });
