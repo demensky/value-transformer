5f5fc0cabd10a31cabd2f1e4b7b80bbf4709aa8c
Dmitry Demensky <dmitry@demensky.com>
2021-07-19 01:12:00 +0300

    Update nullable-transformer.test.ts

--- src/transformer/nullable/nullable-transformer.test.ts
+++ src/transformer/nullable/nullable-transformer.test.ts
@@ -16,48 +16,48 @@ describe('NullableTransformer', () => {
   describe('null value', () => {
     test('compatibleWith', () => {
       expect(transformer.compatibleWith(null)).toBe(true);
-      expect(mockTransformer.compatibleWithCalls).toHaveLength(0);
+      expect(mockTransformer.compatibleWith).toBeCalledTimes(0);
     });
 
     test('fromLiteral', () => {
       expect(transformer.fromLiteral(null)).toBe(null);
-      expect(mockTransformer.fromLiteralCalls).toHaveLength(0);
+      expect(mockTransformer.fromLiteral).toBeCalledTimes(0);
     });
 
     test('toCompactLiteral', () => {
       expect(transformer.toCompactLiteral(null)).toBe(null);
-      expect(mockTransformer.toCompactLiteralCalls).toHaveLength(0);
+      expect(mockTransformer.toCompactLiteral).toBeCalledTimes(0);
     });
 
     test('toLiteral', () => {
       expect(transformer.toCompactLiteral(null)).toBe(null);
-      expect(mockTransformer.toCompactLiteralCalls).toHaveLength(0);
+      expect(mockTransformer.toCompactLiteral).toBeCalledTimes(0);
     });
   });
 
   describe('not null value', () => {
     test('compatibleWith', () => {
       expect(transformer.compatibleWith('param')).toBe(true);
-      expect(mockTransformer.compatibleWithCalls).toHaveLength(1);
-      expect(mockTransformer.compatibleWithCalls[0]).toEqual(['param']);
+      expect(mockTransformer.compatibleWith).toBeCalledTimes(1);
+      expect(mockTransformer.compatibleWith).toBeCalledWith('param');
     });
 
     test('fromLiteral', () => {
       expect(transformer.fromLiteral('param')).toBe('data');
-      expect(mockTransformer.fromLiteralCalls).toHaveLength(1);
-      expect(mockTransformer.fromLiteralCalls[0]).toEqual(['param']);
+      expect(mockTransformer.fromLiteral).toBeCalledTimes(1);
+      expect(mockTransformer.fromLiteral).toBeCalledWith('param');
     });
 
     test('toCompactLiteral', () => {
       expect(transformer.toCompactLiteral('data')).toBe('compactLiteral');
-      expect(mockTransformer.toCompactLiteralCalls).toHaveLength(1);
-      expect(mockTransformer.toCompactLiteralCalls[0]).toEqual(['data']);
+      expect(mockTransformer.toCompactLiteral).toBeCalledTimes(1);
+      expect(mockTransformer.toCompactLiteral).toBeCalledWith('data');
     });
 
     test('toLiteral', () => {
       expect(transformer.toLiteral('data')).toBe('literal');
-      expect(mockTransformer.toLiteralCalls).toHaveLength(1);
-      expect(mockTransformer.toLiteralCalls[0]).toEqual(['data']);
+      expect(mockTransformer.toLiteral).toBeCalledTimes(1);
+      expect(mockTransformer.toLiteral).toBeCalledWith('data');
     });
   });
 });
