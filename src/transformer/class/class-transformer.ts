66314a90b3262b270c1ccff79813c72ca35d314e
Dmitry Demensky <dmitry@demensky.com>
2021-09-05 20:41:04 +0300

    chore(class-transformer): Add static fromConstructor method

--- src/transformer/class/class-transformer.ts
+++ src/transformer/class/class-transformer.ts
@@ -1,6 +1,16 @@
 import {ValueTransformer} from '../../base/value-transformer';
 
 export class ClassTransformer<T> extends ValueTransformer<Readonly<T>, T> {
+  public static fromConstructor<T>(
+    _constructor: new (...args: never) => T,
+  ): ClassTransformer<T> {
+    throw new Error('Not implemented');
+  }
+
+  private constructor() {
+    super();
+  }
+
   public compatibleWith(_data: unknown): _data is Readonly<T> {
     throw new Error('Not implemented');
   }
