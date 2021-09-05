c906c187ade4bc8dad2d64a4756112d51127330a
Dmitry Demensky <dmitry@demensky.com>
2021-09-05 20:31:13 +0300

    test(class-transformer): add tests for @field

---
+++ src/transformer/class/field.test.ts
@@ -0,0 +1,79 @@
+import {asMock} from '../mock/as-mock';
+
+import {CLASS_TRANSFORMER_FIELD_TRANSFORMER} from './class-transformer-field-transformer';
+import {CLASS_TRANSFORMER_KEYS} from './class-transformer-keys';
+import {field} from './field';
+
+describe('ClassTransformer @field decorator', () => {
+  test('just class', () => {
+    const transformerA = asMock(true, null, true, null, null);
+    const transformerB = asMock(true, null, true, null, null);
+
+    class Tmp {
+      @field(transformerA) public a: unknown;
+
+      @field(transformerB) public b: unknown;
+    }
+
+    expect(
+      Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, Tmp.prototype),
+    ).toEqual(['a', 'b']);
+
+    expect(
+      Reflect.getOwnMetadata(
+        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+        Tmp.prototype,
+        'a',
+      ),
+    ).toBe(transformerA);
+
+    expect(
+      Reflect.getOwnMetadata(
+        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+        Tmp.prototype,
+        'b',
+      ),
+    ).toBe(transformerB);
+  });
+
+  test('class extend class', () => {
+    const transformerA = asMock(true, null, true, null, null);
+    const transformerB = asMock(true, null, true, null, null);
+
+    abstract class TmpSuper {
+      @field(transformerA) public a: unknown;
+    }
+
+    class TmpRecipient extends TmpSuper {
+      @field(transformerB) public b: unknown;
+    }
+
+    expect(
+      Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, TmpRecipient.prototype),
+    ).toEqual(['a', 'b']);
+
+    expect(
+      Reflect.getOwnMetadata(
+        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+        TmpRecipient.prototype,
+        'a',
+      ),
+    ).not.toBe(transformerA);
+
+    expect(
+      Reflect.getMetadata(
+        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+        TmpRecipient.prototype,
+        'a',
+      ),
+    ).toBe(transformerA);
+
+    expect(
+      Reflect.getOwnMetadata(
+        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+        TmpRecipient.prototype,
+        'b',
+      ),
+    ).toBe(transformerB);
+  });
+});
