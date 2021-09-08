28f8c2fd900dfb831f24cd8bcff545a15bc2580b
Dmitry Demensky <dmitry@demensky.com>
2021-09-08 23:01:52 +0300

    build: reduce rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,21 +4,18 @@ import typescript from 'rollup-plugin-typescript2';
 const config: readonly RollupOptions[] = [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.mjs',
-      format: 'module',
-      sourcemap: true,
-    },
-    plugins: [typescript({tsconfig: './tsconfig.build.json'})],
-  },
-
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.cjs',
-      format: 'commonjs',
-      sourcemap: true,
-    },
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'module',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'commonjs',
+        sourcemap: true,
+      },
+    ],
     plugins: [typescript({tsconfig: './tsconfig.build.json'})],
   },
 ];
