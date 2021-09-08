a1df0616b47443c114ac938859a4efc721c86656
Dmitry Demensky <dmitry@demensky.com>
2021-09-08 23:09:57 +0300

    feat(never-transformer)!: asNever return same instance

--- src/transformer/never/as-never.ts
+++ src/transformer/never/as-never.ts
@@ -1,5 +1,5 @@
 import {NeverTransformer} from './never-transformer';
 
 export function asNever(): NeverTransformer {
-  return new NeverTransformer();
+  return NeverTransformer.SINGLE;
 }
--- src/transformer/never/never-transformer.ts
+++ src/transformer/never/never-transformer.ts
@@ -3,6 +3,12 @@ import {DeserializationNeverError} from '../../error/deserialization-never-error
 import {SerializationNeverError} from '../../error/serialization-never-error';
 
 export class NeverTransformer extends ValueTransformer<never, never> {
+  public static readonly SINGLE = new NeverTransformer();
+
+  private constructor() {
+    super();
+  }
+
   public compatibleWith(_data: unknown): _data is never {
     return false;
   }
