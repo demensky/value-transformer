0e8188c7f36555d95aebdf6fbaa23a8f9dd7b114
Dmitry Demensky <dmitry@demensky.com>
2021-12-24 19:14:21 +0200

    test: @field exceptions

--- src/transformer/class/field.test.ts
+++ src/transformer/class/field.test.ts
@@ -1,81 +1,112 @@
 import 'reflect-metadata';
 
 import {asMock} from '../mock/as-mock';
+import type {MockTransformer} from '../mock/mock-transformer';
 
 import {CLASS_TRANSFORMER_FIELD_TRANSFORMER} from './class-transformer-field-transformer';
 import {CLASS_TRANSFORMER_KEYS} from './class-transformer-keys';
 import {field} from './field';
 
 describe('ClassTransformer @field decorator', () => {
-  test('just class', () => {
-    const transformerA = asMock(true, null, null, null);
-    const transformerB = asMock(true, null, null, null);
+  let transformerA: MockTransformer<unknown, unknown>;
+  let transformerB: MockTransformer<unknown, unknown>;
 
-    class Tmp {
-      @field(transformerA) public a: unknown;
-
-      @field(transformerB) public b: unknown;
-    }
-
-    expect(
-      Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, Tmp.prototype),
-    ).toStrictEqual(['a', 'b']);
-
-    expect(
-      Reflect.getOwnMetadata(
-        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
-        Tmp.prototype,
-        'a',
-      ),
-    ).toBe(transformerA);
-
-    expect(
-      Reflect.getOwnMetadata(
-        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
-        Tmp.prototype,
-        'b',
-      ),
-    ).toBe(transformerB);
+  beforeEach(() => {
+    transformerA = asMock(true, null, null, null);
+    transformerB = asMock(true, null, null, null);
   });
 
-  test('class extend class', () => {
-    const transformerA = asMock(true, null, null, null);
-    const transformerB = asMock(true, null, null, null);
+  describe('just class', () => {
+    test('successfully', () => {
+      class Tmp {
+        @field(transformerA) public a: unknown;
 
-    abstract class TmpSuper {
-      @field(transformerA) public a: unknown;
-    }
+        @field(transformerB) public b: unknown;
+      }
 
-    class TmpRecipient extends TmpSuper {
-      @field(transformerB) public b: unknown;
-    }
+      expect(
+        Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, Tmp.prototype),
+      ).toStrictEqual(['a', 'b']);
 
-    expect(
-      Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, TmpRecipient.prototype),
-    ).toStrictEqual(['a', 'b']);
+      expect(
+        Reflect.getOwnMetadata(
+          CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+          Tmp.prototype,
+          'a',
+        ),
+      ).toBe(transformerA);
 
-    expect(
-      Reflect.getOwnMetadata(
-        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
-        TmpRecipient.prototype,
-        'a',
-      ),
-    ).not.toBe(transformerA);
+      expect(
+        Reflect.getOwnMetadata(
+          CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+          Tmp.prototype,
+          'b',
+        ),
+      ).toBe(transformerB);
+    });
 
-    expect(
-      Reflect.getMetadata(
-        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
-        TmpRecipient.prototype,
-        'a',
-      ),
-    ).toBe(transformerA);
+    test('throw error if multiple decorators on same field', () => {
+      expect(() => {
+        class Tmp {
+          @field(transformerA) @field(transformerB) public a: unknown;
+        }
 
-    expect(
-      Reflect.getOwnMetadata(
-        CLASS_TRANSFORMER_FIELD_TRANSFORMER,
-        TmpRecipient.prototype,
-        'b',
-      ),
-    ).toBe(transformerB);
+        void Tmp;
+      }).toThrow();
+    });
+  });
+
+  describe('class extends class', () => {
+    test('successfully', () => {
+      abstract class TmpSuper {
+        @field(transformerA) public a: unknown;
+      }
+
+      class TmpRecipient extends TmpSuper {
+        @field(transformerB) public b: unknown;
+      }
+
+      expect(
+        Reflect.getOwnMetadata(CLASS_TRANSFORMER_KEYS, TmpRecipient.prototype),
+      ).toStrictEqual(['a', 'b']);
+
+      expect(
+        Reflect.getOwnMetadata(
+          CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+          TmpRecipient.prototype,
+          'a',
+        ),
+      ).not.toBe(transformerA);
+
+      expect(
+        Reflect.getMetadata(
+          CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+          TmpRecipient.prototype,
+          'a',
+        ),
+      ).toBe(transformerA);
+
+      expect(
+        Reflect.getOwnMetadata(
+          CLASS_TRANSFORMER_FIELD_TRANSFORMER,
+          TmpRecipient.prototype,
+          'b',
+        ),
+      ).toBe(transformerB);
+    });
+
+    test('throw error if multiple decorators on same field', () => {
+      abstract class TmpSuper {
+        @field(transformerA) public a: unknown;
+      }
+
+      expect(() => {
+        class TmpRecipient extends TmpSuper {
+          @field(transformerB) public override a: unknown;
+        }
+
+        void TmpRecipient;
+      }).toThrow();
+    });
   });
 });
