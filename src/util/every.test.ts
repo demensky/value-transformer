3cdae5a60bf5c446396a4fb3047da5ebeb8f5606
Dmitry Demensky <dmitry@demensky.com>
2021-12-25 16:54:31 +0200

    test(util): every

---
+++ src/util/every.test.ts
@@ -0,0 +1,81 @@
+import {every} from './every';
+
+describe('every', () => {
+  let predicate: jest.Mock<boolean, [item: string]>;
+
+  beforeEach(() => {
+    predicate = jest.fn<boolean, [item: string]>();
+  });
+
+  test('no items', () => {
+    expect(every([], predicate)).toBe(true);
+
+    expect(predicate.mock.calls).toHaveLength(0);
+  });
+
+  describe('one item', () => {
+    test('non-matching', () => {
+      predicate.mockReturnValue(false);
+
+      expect(every(['foo'], predicate)).toBe(false);
+
+      expect(predicate).toHaveBeenCalledTimes(1);
+      expect(predicate).toHaveBeenCalledWith('foo');
+    });
+
+    test('matching', () => {
+      predicate.mockReturnValue(true);
+
+      expect(every(['foo'], predicate)).toBe(true);
+
+      expect(predicate).toHaveBeenCalledTimes(1);
+      expect(predicate).toHaveBeenCalledWith('foo');
+    });
+  });
+
+  describe('many items', () => {
+    test('non-matching', () => {
+      predicate.mockReturnValue(false);
+
+      expect(every(['foo', 'bar', 'baz'], predicate)).toBe(false);
+
+      expect(predicate).toHaveBeenCalledTimes(1);
+      expect(predicate).toHaveBeenCalledWith('foo');
+    });
+
+    test('matching only first', () => {
+      predicate.mockReturnValueOnce(true).mockReturnValue(false);
+
+      expect(every(['foo', 'bar', 'baz'], predicate)).toBe(false);
+
+      expect(predicate).toHaveBeenCalledTimes(2);
+      expect(predicate).toHaveBeenNthCalledWith(1, 'foo');
+      expect(predicate).toHaveBeenNthCalledWith(2, 'bar');
+    });
+
+    test('matching first and second', () => {
+      predicate
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(true)
+        .mockReturnValue(false);
+
+      expect(every(['foo', 'bar', 'baz'], predicate)).toBe(false);
+
+      expect(predicate).toHaveBeenCalledTimes(3);
+      expect(predicate).toHaveBeenNthCalledWith(1, 'foo');
+      expect(predicate).toHaveBeenNthCalledWith(2, 'bar');
+      expect(predicate).toHaveBeenNthCalledWith(3, 'baz');
+    });
+
+    test('matching every', () => {
+      predicate.mockReturnValue(true);
+
+      expect(every(['foo', 'bar', 'baz'], predicate)).toBe(true);
+
+      expect(predicate).toHaveBeenCalledTimes(3);
+      expect(predicate).toHaveBeenNthCalledWith(1, 'foo');
+      expect(predicate).toHaveBeenNthCalledWith(2, 'bar');
+      expect(predicate).toHaveBeenNthCalledWith(3, 'baz');
+    });
+  });
+});
