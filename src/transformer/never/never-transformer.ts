366d45f50f3e3b16d2f441333ceef9f90f3682ae
Dmitry Demensky <dmitry@demensky.com>
2021-12-31 16:18:37 +0200

    docs(never-transformer): basic doc

--- src/transformer/never/never-transformer.ts
+++ src/transformer/never/never-transformer.ts
@@ -2,6 +2,11 @@ import {ValueTransformer} from '../../base/value-transformer';
 import {DeserializationNeverError} from '../../error/deserialization-never-error';
 import {SerializationNeverError} from '../../error/serialization-never-error';
 
+/**
+ * Handles no value (any attempt to serialize or deserialize will throw an
+ * exception).
+ * @see {@link asNever} alias
+ */
 export class NeverTransformer extends ValueTransformer<never, never> {
   public constructor() {
     super();
