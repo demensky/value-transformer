e6d93aee413061f13c2d757ea5bd2d5d9ad4aa1d
demensky <dmitry@demensky.com>
2022-06-13 17:20:50 +0300

    test: avoid SyncBufferDeserializer in macroDecoderThrow

--- test-util/macro-decoder-throw.ts
+++ test-util/macro-decoder-throw.ts
@@ -1,16 +1,32 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import type {ExecutionContext} from 'ava';
 
 import type {DecoderGenerator} from '../src/index.js';
-import {InvalidBufferValueError, SyncBufferDeserializer} from '../src/index.js';
-import {map} from '../src/util/map.js';
+import {InvalidBufferValueError} from '../src/index.js';
 
 export function macroDecoderThrow(
   t: ExecutionContext<DecoderGenerator<unknown>>,
   chunks: readonly (readonly number[])[],
 ): void {
   t.throws(() => {
-    SyncBufferDeserializer.from(
-      map(chunks, (elements) => new Uint8Array(elements)),
-    ).finalRead<unknown>(t.context);
+    let request: IteratorResult<number, unknown> = t.context.next();
+    let index = 0;
+
+    for (; request.done !== true && index < chunks.length; index++) {
+      if (
+        !t.is(
+          request.value,
+          chunks[index]!.length,
+          `Wrong length of chunk ${index}`,
+        )
+      ) {
+        return;
+      }
+
+      request = t.context.next(
+        new DataView(new Uint8Array(chunks[index]!).buffer),
+      );
+    }
   }, new InvalidBufferValueError());
 }
