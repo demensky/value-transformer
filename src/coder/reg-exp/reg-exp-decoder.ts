90e628765179271a411a9373106c2d15bf7098d1
demensky <dmitry@demensky.com>
2022-07-29 01:08:25 +0300

    fix(coder): wrap exception in InvalidBufferValueError

--- src/coder/reg-exp/reg-exp-decoder.ts
+++ src/coder/reg-exp/reg-exp-decoder.ts
@@ -1,6 +1,16 @@
+import {InvalidBufferValueError} from '../../error/invalid-buffer-value-error.js';
 import type {DecoderGenerator} from '../../type/decoder-generator.js';
 import {stringDecoder} from '../string/string-decoder.js';
 
 export function* regExpDecoder(): DecoderGenerator<RegExp> {
-  return new RegExp(yield* stringDecoder(), yield* stringDecoder());
+  const pattern = yield* stringDecoder();
+  const flags = yield* stringDecoder();
+
+  try {
+    return new RegExp(pattern, flags);
+  } catch (cause) {
+    // TODO remove "as"
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    throw new InvalidBufferValueError('', {cause} as ErrorOptions);
+  }
 }
