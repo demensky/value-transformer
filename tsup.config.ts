002a42e972dcff802f447d165b009a7e088aca9d
demensky <dmitry@demensky.com>
2022-10-21 23:55:55 +0300

    build: use tsup instead esbuild

---
+++ tsup.config.ts
@@ -0,0 +1,27 @@
+import {defineConfig} from 'tsup';
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    target: 'ES2022',
+    format: ['esm'],
+    sourcemap: true,
+    clean: true,
+  },
+  {
+    entry: ['src/index.ts'],
+    outExtension: () => ({js: '.dev.js'}),
+    target: 'ES2022',
+    format: ['esm'],
+    sourcemap: true,
+    clean: true,
+    esbuildOptions: (options) => {
+      options.drop = ['console'];
+    },
+  },
+  {
+    entry: ['src/index.ts'],
+    clean: true,
+    dts: {only: true},
+  },
+]);
