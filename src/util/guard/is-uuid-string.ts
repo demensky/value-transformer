be5714c9ca96c2b5e17337f98d3cbcde20fe8c46
demensky <dmitry@demensky.com>
2023-06-28 15:14:45 +0300

    refactor(util): uuid string check for performance
    Updated the function to check if a string is a valid UUID.
    The implementation has been changed from using a regular expression
    to a function that manually checks the structure of the string.
    This change improves the function's performance by avoiding
    the overhead of a regular expression, making it faster and
    more efficient for high volume data checks.

--- src/util/guard/is-uuid-string.ts
+++ src/util/guard/is-uuid-string.ts
@@ -1,11 +1,39 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
 import type {UuidString} from '../../type/uuid-string.js';
 
-const UUID_REGEX = /^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/;
+const UUID_LENGTH = 36;
+const HEX_LOWER_BOUND = 48;
+const HEX_UPPER_BOUND = 57;
+const ALPHABET_LOWER = 97;
+const ALPHABET_UPPER = 102;
+const HYPHEN_CHAR_CODE = 45;
 
-/**
- * Checks that `value` is: a string representation of uuid, all characters are
- * lowercase and does not contain whitespace.
- */
+function isHexadecimal(str: string, start: number, length: number): boolean {
+  for (let i = start; i < length; i++) {
+    const code: number = str.charCodeAt(i);
+
+    if (
+      !(code >= HEX_LOWER_BOUND && code <= HEX_UPPER_BOUND) && // numeric (0-9)
+      !(code >= ALPHABET_LOWER && code <= ALPHABET_UPPER) // alphabets (a-f)
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function isUuidString<T extends UuidString>(value: string): value is T {
-  return UUID_REGEX.test(value);
+  return (
+    value.length === UUID_LENGTH &&
+    isHexadecimal(value, 0, 8) &&
+    value.charCodeAt(8) === HYPHEN_CHAR_CODE &&
+    isHexadecimal(value, 9, 13) &&
+    value.charCodeAt(13) === HYPHEN_CHAR_CODE &&
+    isHexadecimal(value, 14, 18) &&
+    value.charCodeAt(18) === HYPHEN_CHAR_CODE &&
+    isHexadecimal(value, 19, 23) &&
+    value.charCodeAt(23) === HYPHEN_CHAR_CODE &&
+    isHexadecimal(value, 24, UUID_LENGTH)
+  );
 }
