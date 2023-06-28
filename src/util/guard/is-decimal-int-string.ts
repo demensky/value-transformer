d01af56d6ec8d78a8ac4bf9f757997b212be4be6
demensky <dmitry@demensky.com>
2023-06-28 13:51:48 +0300

    refactor(util): `isDecimalIntString` function
    Replaced the regular expression check with a more efficient looping
    mechanism in the `isDecimalIntString` function to improve performance.
    This ensures decimal integers string verification is conducted character
    by character which eliminates unnecessary computation.

--- src/util/guard/is-decimal-int-string.ts
+++ src/util/guard/is-decimal-int-string.ts
@@ -1,6 +1,34 @@
-const IS_DECIMAL = /^(?:0|-?[1-9]\d*)$/;
+const ASCII_ZERO = 48;
+const ASCII_NINE = 57;
+const ASCII_MINUS = 45;
 
-// TODO replace regexp to simple loop check
 export function isDecimalIntString(value: string): boolean {
-  return IS_DECIMAL.test(value);
+  if (value.length === 0) {
+    return false;
+  }
+
+  const initialCharCode = value.charCodeAt(0);
+
+  let start = 0;
+
+  if (initialCharCode === ASCII_MINUS) {
+    // is '-'
+    if (value.length === 1 || value.charCodeAt(1) === ASCII_ZERO) {
+      return false; // '-' cannot be standalone or followed by '0'
+    }
+    start = 1;
+  } else if (initialCharCode === ASCII_ZERO) {
+    return value.length === 1; // '0' can only be standalone, not leading
+  }
+
+  for (let i = start; i < value.length; i++) {
+    const charCode = value.charCodeAt(i);
+
+    if (charCode < ASCII_ZERO || charCode > ASCII_NINE) {
+      // not between '0' and '9'
+      return false;
+    }
+  }
+
+  return true;
 }
